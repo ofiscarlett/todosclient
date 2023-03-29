83695baf7269258cf31d62feebd388dc41464af3
Cheng Shufen <ophelia.scarlett007@gmail.com>
2023-03-29 10:00:01 +0300

    new client side

---
+++ ts/index.ts
@@ -0,0 +1,115 @@
+//const list = document.getElementById('todolist') as HTMLUListElement;
+//const input = document.getElementById('newtodo') as HTMLInputElement;
+const BACKED_ROOT_URL = 'http://localhost:3001';
+//question, it should be the  .ts not js
+import { Task } from "./class/Task.js";
+import { Todos } from "./class/Todos.js";
+
+const todos = new Todos(BACKED_ROOT_URL);
+
+const list= <HTMLUListElement>document.querySelector('#todolist');
+const input= <HTMLInputElement>document.querySelector('#newtodo');
+
+//const list=<HTMLUListElement>document.querySelector('#todolist');
+input.disabled = true;
+
+//at todos file, it is getTask, not getTasks
+todos.getTasks().then((tasks: Array<Task>)=>{
+  tasks.forEach((task)=>{
+    renderTask(task)
+  })
+  input.disabled = false;
+}).catch(error =>{
+  alert(error)
+});
+
+
+/* fetch(BACKED_ROOT_URL)
+.then(response => response.json())
+.then((response) => {
+  response.forEach(node=> {
+    renderTask(node.description)
+  });
+  input.disabled = false;
+},(error) =>{
+  alert(error)
+}) */
+
+/* input.addEventListener('keypress', event=>{
+    if(event.key === "Enter") {
+        event.preventDefault()
+        const text = input.value.trim() 
+        if(text !="") {
+            const list_item = document.createElement('li');
+            list_item.setAttribute("class", "list-group-item");
+            list_item.innerHTML = text;
+            list.append("list_item");
+            input.value = "";
+
+        }
+
+    }
+}) */
+
+input.addEventListener('keypress', event => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      const text = input.value.trim();
+      if (text !== '') {
+        todos.addTask(text).then((task) => {
+          input.value='';
+          input.focus();
+          renderTask(<Task>task);
+        //const json= JSON.stringify({description: text})
+        //fetch(BACKED_ROOT_URL + '/new', {
+          //method: 'POST',
+          //headers: {
+            //'Content-Type': 'application/json'
+          //},
+          //body: json
+        })
+        /* .then(response=>response.json())
+        .then((response)=>{ */
+          // renderTask(text)
+          // input.value = ''
+        }
+        //const list_item = document.createElement('li');
+        //list_item.setAttribute('class', 'list-group-item');
+        //list_item.innerHTML = text;
+        //list.append(list_item);
+        //input.value = '';
+        event.preventDefault();
+      }
+    }
+  );
+  
+  const renderTask =(task:Task) => {
+    const list_item = document.createElement('li')
+    list_item.setAttribute('class', 'list-group-item')
+    list_item.setAttribute('data-key', task.id.toString())
+    //list_item.innerHTML = task.text
+    renderSpan(list_item,task.text)
+    renderLink(list_item,task.id )
+    list.append(list_item)
+  }
+
+  const renderSpan = (list_item:HTMLLIElement, text:string) => {
+    const span = list_item.appendChild(document.createElement('span'))
+    span.innerHTML = text
+  }
+
+  const renderLink = (list_item:HTMLLIElement, id:number) => {
+    const link = list_item.appendChild(document.createElement('a'))
+    link.innerHTML= '<i class = "bi bi-trash"> </i>'
+    link.setAttribute('style', 'float:right')
+    link.addEventListener('click', event => {
+      todos.removeTask(id).then((id) => {
+        const elementToRemove: HTMLLIElement = document.querySelector(`[data-key="${id}"]`)
+        if (elementToRemove) {
+          list.removeChild(elementToRemove)
+        }
+      }).catch(error => {
+        alert(error)
+      })
+    })
+  }
